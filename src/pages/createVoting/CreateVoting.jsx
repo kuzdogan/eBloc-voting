7a4204ca1f8177d9cb87e340ee206c8660a9d214
kuzdogan <kaanuzdogan@hotmail.com>
2017-12-31 12:50:05 +0300

    Create Voting form with Bootstrap

--- src/pages/createVoting/CreateVoting.jsx
+++ src/pages/createVoting/CreateVoting.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Button} from 'react-bootstrap'
+import {Button, Form, FormGroup, FormControl, ControlLabel, Glyphicon, Grid, Row, Col} from 'react-bootstrap'
 import SmartVotingContract from '../../../build/contracts/SmartVoting.json'
 import getWeb3 from '../../utils/getWeb3'
 import Datetime from 'react-datetime'
@@ -12,9 +12,11 @@ class CreateVoting extends Component{
     this.state = {
       web3: null,
       owner: null,
+      voters: [{ address: ''}],
+      candidates: [{ address: ''}],
       smartVotingInstance: null
     }
-    this.instantiateContract = this.instantiateContract.bind(this)
+    this.instantiateContract = this.instantiateContract.bind(this);
   }
 
   componentWillMount() {
@@ -53,20 +55,127 @@ class CreateVoting extends Component{
 	  console.log("NOT YET");
   }
 
+  // Voter Handlers
+  handleVoterChange = (idx) => (evt) =>{
+  	const newVoters = this.state.voters.map((voter, sidx) => {
+      if (idx !== sidx) return voter;
+      return { ...voter, address: evt.target.value };
+    });
+    this.setState({ voters: newVoters });
+  }
+  handleAddVoter = () => {
+    this.setState({ voters: this.state.voters.concat([{ address: '' }]) });
+  }
+  handleRemoveVoter = (idx) => () => {
+    this.setState({ voters: this.state.voters.filter((v, sidx) => idx !== sidx) });
+  }  
+
+  // Candidate Handlers
+  handleCandidateChange = (idx) => (evt) =>{
+  	const newCandidates = this.state.candidates.map((candidate, sidx) => {
+      if (idx !== sidx) return candidate;
+      return { ...candidate, address: evt.target.value };
+    });
+    this.setState({ candidates: newCandidates });
+  }
+  handleAddCandidate = () => {
+    this.setState({ candidates: this.state.candidates.concat([{ address: '' }]) });
+  }
+  handleRemoveCandidate = (idx) => () => {
+    this.setState({ candidates: this.state.candidates.filter((v, sidx) => idx !== sidx) });
+  }  
+
 	render(){
 		return(
 		<div className="page">
-			<h1> Here you can create votings</h1>
-			<p> Select starting date </p>
-			<Datetime defaultValue={new Date()}/>
-			<p> Select end date </p>
-			<Datetime defaultValue={new Date()}/>
-			<Button bsStyle="primary" onClick={this.instantiateContract}>
-			Create a voting
-			</Button>
-			<div className="contract">
-				<p> Owner of the contract is: {this.state.owner} </p>
-			</div>
+			<Grid>
+				<h1> Here you can create votings</h1>
+				<Form>
+					<FormGroup>
+						<Row>
+							<p> Enter voter addresses </p>	
+						</Row>
+						{this.state.voters.map((voter, idx) => (
+		          <Row className="voter-input">
+		          	<Col md={6}>
+			            <FormControl
+			              type="text"
+			              placeholder={`Voter #${idx + 1} address`}
+			              value={voter.address}
+			              onChange={this.handleVoterChange(idx)}
+			            />
+			          </Col>
+			          <Col>
+			            <Button bsStyle="danger" onClick={this.handleRemoveVoter(idx)}>
+			            	<Glyphicon glyph="trash"/>
+		            	</Button>
+	            	</Col>
+		          </Row>
+		        ))}
+	        	<Button bsStyle="primary" onClick={this.handleAddVoter}>Add Voter</Button>
+	        </FormGroup>
+	        <FormGroup>
+						<Row>
+							<p> Enter candidate addresses </p>	
+						</Row>
+
+						{this.state.candidates.map((candidate, idx) => (
+		          <Row className="voter-input">
+		          	<Col md={6}>
+			            <FormControl
+			              type="text"
+			              placeholder={`Candidate #${idx + 1} address`}
+			              value={candidate.address}
+			              onChange={this.handleCandidateChange(idx)}
+			            />
+			          </Col>
+			          <Col>
+			            <Button bsStyle="danger" onClick={this.handleRemoveCandidate(idx)}>
+			            	<Glyphicon glyph="trash"/>
+		            	</Button>
+	            	</Col>
+		          </Row>
+		        ))}
+        		<Button bsStyle="primary" onClick={this.handleAddCandidate}>Add Candidate</Button>
+		      </FormGroup>
+		      <FormGroup inline>
+		        <Row>
+	        		<ControlLabel> Select starting date and time</ControlLabel>
+	        	</Row>
+	        	<Row>      
+		        	<Col md={6}>
+								<Datetime defaultValue={new Date()}/>
+		        	</Col>
+						</Row>
+		        <Row>
+	        		<ControlLabel> Select end date and time</ControlLabel>
+	        	</Row>
+	        	<Row>      
+		        	<Col md={6}>
+								<Datetime defaultValue={new Date()}/>
+		        	</Col>
+						</Row>
+						<Button bsStyle="primary" onClick={this.instantiateContract}>
+						Create a voting
+						</Button>
+					</FormGroup>
+				</Form>
+				<div className="contract-info">
+					<p> Owner of the contract is: {this.state.owner} </p>
+					<h2> Voters are</h2>
+						{this.state.voters.map((voter, idx) => (
+		          <Row className="voter-{idx}">
+		          	<p> Voter {idx}: {voter.address} </p>
+		          </Row>
+		        ))}
+					<h2> Candidates are</h2>
+						{this.state.candidates.map((candidate, idx) => (
+		          <Row className="candidate-{idx}">
+		          	<p> Candidate {idx}: {candidate.address} </p>
+		          </Row>
+		        ))}
+				</div>
+			</Grid>
 		</div>
 		)
 	}
