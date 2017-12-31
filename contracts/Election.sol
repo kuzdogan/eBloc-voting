b2074760215b558c5c993430625418766d339bd0
kirec <ali.kirecligol@urbanstat.com>
2017-12-31 15:01:53 +0300

    smart voting contract update

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,61 +1,75 @@
-pragma solidity ^0.4.6;
+pragma solidity ^0.4.18;
+// We have to specify what version of compiler this code will compile with
 
 contract SmartVoting {
-    struct Voter {
-        bool voted;
-    }
-    struct Candidate {
-        uint numberOfVotes;
-    }
-    struct Election {
-        uint startTimeOfElection;
-        uint lengthOfElection;
-        uint deadlineOfElection;
-        address[] electionVoters;
-        address[] electionCandidates;
-    }
-    address public owner;
-    uint public currNumberOfElections;
-    mapping(address => Election) elections;
-    mapping(address => Voter) allVoters;
-    mapping(address => Candidate) allCandidates;
-    mapping(address => uint[]) electionResults;
-    function SmartVoting() public {
-        owner = msg.sender;
-        currNumberOfElections = uint(0);
-    }
-    function createElection(uint startTime, uint deadline,address[] voterAddresses, address[] candidateAddresses) public returns(bool success) {
-        Election memory e;
-        for (uint i = 0;i < voterAddresses.length;i ++) {
-            allVoters[voterAddresses[i]] = Voter({ voted: false});
-        }
-        e.electionVoters = voterAddresses;
-        for (uint j = 0;j < candidateAddresses.length;j ++) {
-            allCandidates[candidateAddresses[j]] = Candidate({numberOfVotes: uint(0)});
-        }
-        e.electionCandidates = candidateAddresses;
-        e.startTimeOfElection = startTime;
-        e.lengthOfElection = deadline - startTime;
-        e.deadlineOfElection = deadline;
-        elections[msg.sender] = e;
-        currNumberOfElections ++;
-        return true;
-    }
-    function vote (address electionAddress, address candidateAddress) public returns(bool success) {
-        if (elections[electionAddress].startTimeOfElection < block.timestamp && elections[electionAddress].deadlineOfElection > block.timestamp) {
-            allCandidates[candidateAddress].numberOfVotes ++;
-            return true;
-        }
-        return false;
-    }
-    function getElectionResults(address electionAddress) internal returns(uint[] cnds) {
-        uint[] a;
-        if (elections[electionAddress].deadlineOfElection > block.timestamp) {
-            return a;
-        }
-        for (uint i = 0;i < elections[electionAddress].electionCandidates.length; i++) {
-            electionResults[electionAddress].push(allCandidates[elections[electionAddress].electionCandidates[i]].numberOfVotes);
-        }
-        return electionResults[electionAddress];        
-    }
+  uint public numberOfElections;
+  struct Voter {
+      address voterAddress;
+      bool voted;
+      uint electionId;
+  }
+  
+  struct Election {
+      uint electionId;
+      uint start;
+      uint end;
+      bytes32[] candidateList;
+  }
+  
+  mapping(uint => mapping(bytes32 => uint)) votesReceived;
+  mapping(address => Voter) voters;
+  mapping(uint => Election) elections;
+  
+  function getElectionId(address va) public view returns (uint) {
+    return voters[va].electionId;
+  }
+  
+  function voteFor(bytes32 candidateName, uint electionId) public {
+      require(isVoted(msg.sender) == false && isActive(electionId) == true);
+      voters[msg.sender].voted = true;
+      votesReceived[electionId][candidateName] += 1;
+  }
+  
+  function isVoted(address voterAddress) view public returns(bool) {
+      return voters[voterAddress].voted;
+  }
+  
+  function isActive(uint electionId) view public returns(bool) {
+      return elections[electionId].start < block.timestamp && elections[electionId].end > block.timestamp;
+  }
+  
+  function getElectionStart(uint electionId) public view returns(uint) {
+      return elections[electionId].start;
+  }
+  
+  function getElectionEnd(uint electionId) public view returns(uint) {
+      return elections[electionId].end;
+  }
+  
+  function getElectionSecondCand(uint electionId) public view returns(bytes32) {
+      return elections[electionId].candidateList[1];
+  }
+
+  function createElection(bytes32[] cands, address[] vl, uint begin, uint deadline) public returns(uint) {
+      numberOfElections ++;
+      elections[numberOfElections] = Election({
+         electionId: numberOfElections,
+         candidateList: cands,
+         start: begin,
+         end: deadline
+      });
+      for (uint i=0;i < vl.length;i ++) {
+          voters[vl[i]] = Voter({
+              voterAddress: vl[i],
+              voted: false,
+              electionId: numberOfElections
+          });
+      }
+      return numberOfElections;
+  }
+  
+  function getVoteNumber(uint electionId, bytes32 candidateName) public view returns(uint) {
+      require(isActive(electionId) == false);
+      return votesReceived[electionId][candidateName];
+  }
 }
