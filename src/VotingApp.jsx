a6387cba55ad4c855c0d44f43eefd7fcbca2cc8a
kuzdogan <kaanuzdogan@hotmail.com>
2018-01-04 01:58:09 +0300

    Election Verification Functionality

--- src/VotingApp.jsx
+++ src/VotingApp.jsx
@@ -10,7 +10,7 @@ class VotingApp extends Component {
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Samrt Voting</a>
+            <a href="#" className="pure-menu-heading pure-menu-link">Smart Voting</a>
         </nav>
 
         <main className="container">
@@ -20,6 +20,7 @@ class VotingApp extends Component {
               <p>Please select to create a voting or vote in an existing election</p>
               <Button bsStyle="primary" href="/create" style={{margin: '5px'}}> Create Election </Button>
               <Button bsStyle="primary" href="/vote" style={{margin: '5px'}}> Vote in an Election </Button>
+              <Button bsStyle="primary" href="/verify" style={{margin: '5px'}}> Verify Election</Button>
             </div>
           </div>
         </main>
